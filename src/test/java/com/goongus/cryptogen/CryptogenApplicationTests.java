726930f26647ae5277f168079dc3626732be0655
Jinwoo An <jinwoo.gus.an@gmail.com>
2021-03-02 07:34:23 +0000

    Init prj

---
+++ src/test/java/com/goongus/cryptogen/CryptogenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goongus.cryptogen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptogenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
